4cc1bad5996caa0caad512a8cebd181821bfde3d
Tom Larsson <tmlsn@hotmail.com>
2025-09-19 20:09:21 +0200

    fix double provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { TaskPage, ColumnPage, BoardPage } from '@pages';
-import { BoardProvider } from '@context';
 import './styles/App.css';
 
 export default function App() {
   return (
-    <BoardProvider>
-      <Routes>
-        <Route path="/" element={<BoardPage />} />
-        <Route path="/column/:columnId" element={<ColumnPage />} />
-        <Route path="/column/:columnId/task/:taskId" element={<TaskPage />} />
-      </Routes>
-    </BoardProvider>
+    <Routes>
+      <Route path="/" element={<BoardPage />} />
+      <Route path="/column/:columnId" element={<ColumnPage />} />
+      <Route path="/column/:columnId/task/:taskId" element={<TaskPage />} />
+    </Routes>
   );
 }
